cd8b42551574020de9346f97a2545dc54c502c19
˜Nicolas <nicolasjap@hotmail.com>
2023-05-13 04:03:42 -0300

    update

--- I am Hero/I am Hero/ViewController.swift
+++ I am Hero/I am Hero/ViewController.swift
@@ -8,11 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBAction func switchImage(_ sender: UISwitch) {
+        if(sender.isOn == false)
+        {
+            imageView.image = UIImage(imageLiteralResourceName: "Spider-Man Headspidey")
+            text.text = "I`m SpiderMan"
+        }else{
+            imageView.image = UIImage(imageLiteralResourceName: "Spider-Man Headspidey_contrast")
+            text.text = "I`m SpiderMan"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBOutlet weak var text: UILabel!
+    
+    @IBAction func btnHuman(_ sender: UIButton) {
+        imageView.image = UIImage(imageLiteralResourceName: "Human Headhuman")
+        
+        text.text = "I`m Peter Parker"
+        text.textColor = UIColor(named: "red")
+        
 
+        
+    }
+    
 }
 
