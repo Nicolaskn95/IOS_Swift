ecfc0455da28f474b730c6b32bac03d6a33abfa8
Nicolas Nagano <nicolasjap@hotmail>
2023-06-06 15:14:47 -0300

    add_lines

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -11,15 +11,43 @@ import Foundation
 
 class ViewController: UIViewController {
     
-    @IBAction func btnSoft(_ sender: UIButton) {
-        print(sender.titleLabel?.text as Any)
-    }
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
     
-    @IBAction func btnMedium(_ sender: UIButton) {
-        print(sender.titleLabel?.text as Any)
-    }
-    @IBAction func btnHard(_ sender: UIButton) {
-        print(sender.titleLabel?.text as Any)
-    }
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
     
+    @IBAction func hardenessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+  
+        
+       let result = eggTimes[hardness]!
+        
+        print(result)
+        
+        
+        
+//        switch hardness{
+//        case "Soft":
+//            print(eggTimes:0)
+//        case "Medium":
+//            print(eggTimes.1)
+//        case "Hard":
+//            print(eggTimes.2)
+//        default:
+//            print("Error")
+//        }
+        
+//        if(hardness == "Soft"){
+//            print(softTime)
+//        }
+//        else if (hardness == "Medium")
+//        {
+//            print (mediumTime)
+//        }
+//        else
+//        {
+//            print(hardTime)
+//        }
+    }
 }
